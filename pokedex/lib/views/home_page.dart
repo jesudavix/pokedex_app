1171a3b3a404eee3d30ecb34f3156d8525952ad3
Alfred Uribe <160811397+jesudavix@users.noreply.github.com>
2025-05-20 13:38:06 -0500

    Esqueleto, creacion de carpetas para la app

---
+++ pokedex/lib/views/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePagePokemon extends StatelessWidget {
+  const HomePagePokemon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Hola Mundo")
+        ],
+      ),
+    );
+  }
+}
