8c2b9811b0897df3f26fa763337e4de69ee2b79b
Jesus Bovea <jdavid.bovea@gmail.com>
2025-05-22 02:36:36 -0500

    Funcionalidad buscar pokemon por nombre
    Se cambio el blockProvider del padre por un MultiBlocPorovider para acceder a ambos bloc desde las clases hijas y manejar los eventos y los estados

--- pokedex/lib/views/home_page.dart
+++ pokedex/lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/blocs/bloc_search_icon_input/search_bloc.dart';
+import 'package:pokedex/blocs/bloc_ui_pokemon/bloc/pokemon_bloc.dart';
+import 'package:pokedex/repositories/api.dart';
 import 'package:pokedex/widgets/app_bar_home_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,11 +10,14 @@ class HomePagePokemon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SearchBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SearchBloc()),
+        BlocProvider(create: (context) => PokemonBloc(PokemonRepository())..add(ObtenerListaPokemones())),
+      ],
       child: BlocBuilder<SearchBloc, SearchState>(
         builder: (context, state) {
-          return AppBarHome(state: state,);
+          return AppBarHome(state: state);
         },
       ),
     );
--- pokedex/lib/widgets/body_home_page.dart
+++ pokedex/lib/widgets/body_home_page.dart
@@ -3,84 +3,84 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex/blocs/bloc_ui_pokemon/bloc/pokemon_bloc.dart';
 import 'package:pokedex/components/styles/colors.dart';
 import 'package:pokedex/components/styles/styles_text.dart';
-import 'package:pokedex/repositories/api.dart';
+
 
 class BodyHomePage extends StatelessWidget {
   const BodyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create:
-          (context) =>
-              PokemonBloc(PokemonRepository())..add(ObtenerListaPokemones()),
-      child: BlocBuilder<PokemonBloc, PokemonState>(
-        builder: (context, state) {
-          if (state is PokemonCargando) {
-            return const Center(
-              child: CircularProgressIndicator(color: Colors.red),
-            );
-          } else if (state is PokemonCargado) {
-            return GridView.builder(
-              padding: EdgeInsets.only(top: 10, bottom: 10, left: 6, right: 20),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.3,
-                mainAxisSpacing: 12,
-                crossAxisSpacing: 14,
-              ),
-              itemCount: state.pokemons.length,
-              itemBuilder: (context, index) {
-                final colorCard = AppColorStyle.getColor(
-                  state.pokemons[index].types.first,
-                );
-                return Card(
-                  color: colorCard,
-                  elevation: 4,
-                  child: Column(
-                    children: [
-                      Image.network(
-                        state.pokemons[index].imageUrl,
-                        height: 80,
-                        fit: BoxFit.cover,
-                      ),
-                      Text(
-                        state.pokemons[index].name.toUpperCase(),
-                        style: AppTextStyle.pokemonName(),
-                      ),
-                      Wrap(
-                        spacing: 6,
-                        runSpacing: 4,
-                        children: [
-                          ...state.pokemons[index].types.map((type) {
-                            final typeColor = AppColorStyle.getColorParaTipo(type);
-                            return Container(
-                              padding: EdgeInsets.all(8),
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.black, width: 1.5),
-                                borderRadius: BorderRadius.circular(15),
-                                color: typeColor,
+    return BlocBuilder<PokemonBloc, PokemonState>(
+      builder: (context, state) {
+        if (state is PokemonCargando) {
+          return const Center(
+            child: CircularProgressIndicator(color: Colors.red),
+          );
+        } else if (state is PokemonCargado) {
+          return GridView.builder(
+            padding: EdgeInsets.only(top: 10, bottom: 10, left: 6, right: 20),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1.3,
+              mainAxisSpacing: 12,
+              crossAxisSpacing: 14,
+            ),
+            itemCount: state.pokemons.length,
+            itemBuilder: (context, index) {
+              final colorCard = AppColorStyle.getColor(
+                state.pokemons[index].types.first,
+              );
+              return Card(
+                color: colorCard,
+                elevation: 4,
+                child: Column(
+                  children: [
+                    Image.network(
+                      state.pokemons[index].imageUrl,
+                      height: 80,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(
+                      state.pokemons[index].name.toUpperCase(),
+                      style: AppTextStyle.pokemonName(),
+                    ),
+                    Wrap(
+                      spacing: 6,
+                      runSpacing: 4,
+                      children: [
+                        ...state.pokemons[index].types.map((type) {
+                          final typeColor = AppColorStyle.getColorParaTipo(
+                            type,
+                          );
+                          return Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 1.5,
                               ),
-                              child: Text(
-                                type.toUpperCase(),
-                                style: AppTextStyle.pokemonType(),
-                              ),
-                            );
-                          }),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          } else if (state is PokemonError) {
-            return Center(child: Text(state.message));
-          } else {
-            return const SizedBox();
-          }
-        },
-      ),
+                              borderRadius: BorderRadius.circular(15),
+                              color: typeColor,
+                            ),
+                            child: Text(
+                              type.toUpperCase(),
+                              style: AppTextStyle.pokemonType(),
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        } else if (state is PokemonError) {
+          return Center(child: Text(state.message));
+        } else {
+          return const SizedBox();
+        }
+      },
     );
   }
 }
